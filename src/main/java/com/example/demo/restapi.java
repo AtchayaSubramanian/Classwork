49f6750ad68b9e34c6150834196336b0689b216a
AtchayaSubramanian <atchayaachu630@gmail.com>
2023-02-24 14:14:45 +0530

    hello portal

---
+++ src/main/java/com/example/demo/restapi.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restapi {
+@Value("${thank.page}")
+private String testpagestring;
+@GetMapping("/rest")
+public String getName() {
+	return "Welcome to this "+testpagestring;
+}
+}
+
